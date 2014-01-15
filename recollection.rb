c5223de2566418199ee507050fcd5791c0d2463e
GavMo <quavmo@gmail.com>
2014-01-15 13:50:27 -0800

    Renames recollection to Memory recollection#of searches for agents as well as styles log uses Logger, to avoid 'too many open files' error

---
+++ recollection.rb
@@ -0,0 +1,36 @@
+require 'logger'
+
+class Recollection
+  def initialize
+    @sequence = []
+  end
+
+  def record interaction
+    @sequence << interaction
+    log interaction
+  end
+
+  def of entity
+    @sequence.select {|i| i.style == entity || i.agent == entity }
+  end
+
+  private
+
+  def self.logger
+    @logger ||= Logger.new('log').tap do |logger|
+      logger.formatter = ->(_, _, _, msg){"#{msg}\n"}
+    end
+  end
+
+  def log interaction
+    line = [interaction.agent.name, interaction.magnitude, interaction.target.name].join '•'
+    self.class.logger.info line
+  end
+end
+
+class Array
+  def in_toto
+    return 0 if self.none?
+    self.map(&:magnitude).reduce(:+)
+  end
+end
