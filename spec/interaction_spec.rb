fb413124626bdcfa1ac30fcdc8689a12f36c4cf1
GavMo <quavmo@gmail.com>
2014-02-09 16:33:13 -0800

    Pending Interaction spec

---
+++ spec/interaction_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe Interaction do
+  describe "#initialize" do
+    let(:obj) { described_class.new }
+    before do
+      obj.initialize
+    end
+
+    it 'works'
+  end
+end
