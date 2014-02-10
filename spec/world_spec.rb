2eb232937ff6392e503345821c95e24f0c6d4d30
GavMo <quavmo@gmail.com>
2014-02-09 17:27:18 -0800

    Pending world specs

--- spec/world_spec.rb
+++ spec/world_spec.rb
@@ -1,7 +1,76 @@
 require 'spec_helper'
 
 describe World do
-  it 'spins' do
-    World.new.should respond_to :spin
+  before do
+    allow(obj).to receive :attr_accessor
+    allow(obj).to receive :private
+    allow(obj).to receive :attr_accessor
+  end
+
+  describe "#initialize" do
+    let(:obj) { described_class.new }
+    before do
+      obj.initialize
+    end
+
+    it 'works'
+  end
+
+  describe "#advance" do
+    let(:obj) { described_class.new }
+    before do
+      obj.advance
+    end
+
+    it 'works'
+  end
+
+  describe "#spin" do
+    let(:obj) { described_class.new }
+    before do
+      allow(obj).to receive :stats
+      obj.spin
+    end
+    it "calls #stats" do
+      expect(obj).to have_received :stats
+    end
+
+    it 'works'
+  end
+
+  describe "#wealth" do
+    let(:obj) { described_class.new }
+    before do
+      obj.wealth
+    end
+
+    it 'works'
+  end
+
+  describe "#winner" do
+    let(:obj) { described_class.new }
+    before do
+      obj.winner
+    end
+
+    it 'works'
+  end
+
+  describe "#loser" do
+    let(:obj) { described_class.new }
+    before do
+      obj.loser
+    end
+
+    it 'works'
+  end
+
+  describe "#remove" do
+    let(:obj) { described_class.new }
+    before do
+      obj.remove
+    end
+
+    it 'works'
   end
 end
